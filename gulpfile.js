6a1ccd0b88b3be81ca6ef03138b1b0fce63bab41
gaoailing <1058491659@qq.com>
2017-10-29 19:33:30 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,36 +7,20 @@ uglify = require('gulp-uglify'),
     watchPath = require('gulp-watch-path'),
     combiner = require('stream-combiner2');
 
-gulp.task('webserver', function() {
+gulp.task('myserver', function() {
     connect.server({
-        root: './src/index.html',
+        name: 'myserver',
+        root: 'gulp',
         livereload: true,
-        port: 3000
+        port: 3000,
+        host: "localhost",
+        livereload: true
     });
 });
-
-//代码着色与显示错误日志方法，这个方法用到了gulp-util和stream-combiner2插件
-var handleError = function(err) {
-    console.log('\n');
-    gutil.log('fileName: ' + gutil.colors.red(err.fileName));
-    gutil.log('lineNumber: ' + gutil.colors.red(err.lineNumber));
-    gutil.log('message: ' + err.message);
-    gutil.log('plugin: ' + gutil.colors.yellow(err.plugin));
-};
-//js批量压缩任务
-gulp.task('script', function() { //script时自定义的  
-    //将文件的源路径和发布路径赋值给相应变量  
-    var srcJsPath = 'js/*.js';
-    var destJsPath = 'dist/js/';
-    var combined = combiner.obj([
-        gulp.src(srcJsPath), //获取文件源地址  
-        uglify(), //执行压缩  
-        gulp.dest(destJsPath) //将压缩的文件发布到新路径  
-    ]);
-    combined.on('error', handleError); //打印错误日志  
+gulp.task('loadfiles', function() {
+    gulp.src('gulp/**/*.*')
 });
-//
+gulp.watch('gulp/**/*.*', ['loadfiles']);
 
 
-
-gulp.task('default', ['webserver']);
+gulp.task('default', ['myserver']);
